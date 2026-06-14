beb2e02e6bafb06c754dbb2c1720da8c16078cf4
hiroki-sato <hiroki.sato@vitalize.co.jp>
2026-06-14 23:55:30 +0900

    fix: NumField の文字寄せをデフォルト左寄せに
    スコア入力フォームで素点・チップ枚数の値とプレースホルダーが
    右端に張り付いてラベルから離れて見える問題を修正。
    右寄せが必要な場合は align='right' で opt-in できる。

--- components/ui/NumField.tsx
+++ components/ui/NumField.tsx
@@ -3,7 +3,7 @@ type NumFieldProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> &
   suffix?: string
 }
 
-export function NumField({ align = 'right', suffix, className = '', ...props }: NumFieldProps) {
+export function NumField({ align = 'left', suffix, className = '', ...props }: NumFieldProps) {
   return (
     <div
       className={`border-line-strong bg-surface focus-within:border-felt focus-within:ring-felt/10 flex h-10 items-center rounded-md border-2 focus-within:ring-2 ${className}`}
